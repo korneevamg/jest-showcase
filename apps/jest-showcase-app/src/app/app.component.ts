04d2dbc408942c6aabdcdb61579f53be0410f036
Maria Korneeva <korneevamg@gmail.com>
2021-07-30 20:28:36 +0200

    initial commit

---
+++ apps/jest-showcase-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jest-showcase-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'jest-showcase-app';
+}
